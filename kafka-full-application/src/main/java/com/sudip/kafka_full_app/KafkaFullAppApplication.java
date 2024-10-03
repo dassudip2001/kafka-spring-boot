10e7c08dfa40f55744f2249cfc8449f51bda5d1d
Sudip Das <sudip5428@gmail.com>
2024-10-03 20:06:05 +0530

    code commit for kafka full application

---
+++ kafka-full-application/src/main/java/com/sudip/kafka_full_app/KafkaFullAppApplication.java
@@ -0,0 +1,13 @@
+package com.sudip.kafka_full_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaFullAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaFullAppApplication.class, args);
+	}
+
+}
---
+++ kafka-full-application/src/test/java/com/sudip/kafka_full_app/KafkaFullAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudip.kafka_full_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaFullAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
